a087a61047b9f03ab308558a2523ddb16ef7c1ba
znergy <ryanjonesirl@gmail.com>
2017-06-05 10:29:25 -0700

    add the code to save user information to firebase

---
+++ app/src/main/java/com/znergy/firebasepractice/MainActivity.java
@@ -0,0 +1,64 @@
+package com.znergy.firebasepractice;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.znergy.firebasepractice.models.User;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.btnSave) Button btnSave;
+    @Bind(R.id.etFirstName) EditText etFirstName;
+    @Bind(R.id.etLastName) EditText etLastName;
+    @Bind(R.id.etEmail) EditText etEmail;
+    @Bind(R.id.etPhoneNumber) EditText etPhoneNumber;
+
+
+    /** Declaring Firebase variable */
+    private DatabaseReference userAccount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        /** Assigning the Firebase Reference, basically we are telling Firebase where are
+         * starting point should be inside our database. For this use case, we want the
+         * userAccount variable to hold the reference to the users section of our database. */
+        userAccount = FirebaseDatabase
+                .getInstance()
+                .getReference()
+                .child("users");
+
+        btnSave.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == btnSave) {
+            /** We pull the fields and create a User object */
+            String firstName = etFirstName.getText().toString();
+            String lastName = etLastName.getText().toString();
+            String email = etEmail.getText().toString();
+            String phoneNumber = etPhoneNumber.getText().toString();
+            User user = new User(firstName, lastName, email, phoneNumber);
+
+            /** After creating a user object, we then save to firebase by calling the
+             * saveToFirebase() method */
+            saveToFirebase(user);
+        }
+    }
+
+    private void saveToFirebase(User user) {
+        /** Store the user object in our users section of our database */
+        userAccount.push().setValue(user);
+    }
+}
---
+++ app/src/main/java/com/znergy/firebasepractice/models/User.java
@@ -0,0 +1,45 @@
+package com.znergy.firebasepractice.models;
+
+/**
+ * Created by administrator on 6/5/17.
+ */
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+
+    public User(String firstName, String lastName, String email, String phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
+}
